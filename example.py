f22d533508452cd853a42258b11c7b5d08a10243
sjjeong <sjjeong94@gmail.com>
2022-02-16 23:34:15 +0900

    Cityscapes Demo

--- example.py
+++ example.py
@@ -46,5 +46,63 @@ def main():
     engine.train(epoch_begin, epoch_end, True)
 
 
+def test():
+    set_seed(1234)
+
+    learning_rate = 0.0003
+    weight_decay = 0
+    batch_size = 16
+    epoch_begin = 0
+    epoch_end = 100
+
+    dataset = 'Cityscapes'
+    name = 'test_001'
+    dataset_root = f'./{dataset}/data'
+    model_root = f'./{dataset}/model/{name}'
+    result_root = f'./{dataset}/result'
+    image_root = f'./{dataset}/image'
+
+    net = lraspp_mobilenet_v3_large(num_classes=20)
+    device = get_device()
+    optimizer = torch.optim.Adam(
+        net.to(device).parameters(),
+        lr=learning_rate,
+        weight_decay=weight_decay)
+
+    train_loader, val_loader = get_cityscapes(dataset_root, batch_size)
+
+    net.load_state_dict(torch.load(
+        'Cityscapes/model/test_001/test_001_epoch100.pth'))
+
+    print(net)
+    print(device)
+    print(optimizer)
+
+    engine = Engine(
+        name=name,
+        net=net,
+        device=device,
+        optimizer=optimizer,
+        train_loader=train_loader,
+        val_loader=val_loader,
+        model_root=model_root,
+        result_root=result_root,
+        image_root=image_root,
+    )
+
+    engine.evaluate()
+
+
+def demo():
+    net = torchvision.models.segmentation.lraspp_mobilenet_v3_large(
+        num_classes=20)
+    net.load_state_dict(torch.load(
+        'Cityscapes/model/test_001/test_001_epoch100.pth'))
+    cd = CityscapesDemo('Cityscapes/data', net)
+    cd.make_video('output.mp4')
+
+
 if __name__ == '__main__':
-    main()
+    # main()
+    # test()
+    demo()
--- utils.py
+++ utils.py
@@ -1,10 +1,12 @@
 import os
+import cv2
 import random
 import numpy as np
 import torch
 import torch.nn as nn
 import torchvision
 import transforms as T
+from PIL import Image
 from tqdm import tqdm
 
 mapping_20 = {
@@ -113,7 +115,7 @@ def get_cityscapes(root, batch_size):
 def get_miou(y, o, num_classes):
     m = y == o
     iou_list = []
-    for i in range(num_classes):
+    for i in range(1, num_classes):
         myi = m[y == i]
         moi = m[o == i]
         inter = myi.sum()
@@ -215,16 +217,106 @@ class Engine:
     def evaluate(self):
         self.net = self.net.eval()
         miou_list = []
-        with torch.no_grad():
-            for _, (x, y) in enumerate(tqdm(self.val_loader)):
+        for _, (x, y) in enumerate(tqdm(self.val_loader)):
+            with torch.no_grad():
                 lab = y.numpy()
-                x = x.to(self.device) / 255.
+                x = x.to(self.device)
                 y = y.to(self.device)
                 out = self.net(x)['out']
                 pred = torch.argmax(out, 1)
                 pred = pred.cpu().numpy()
                 for i in range(len(lab)):
-                    miou = get_miou(lab[i], pred[i], 34)
+                    miou = get_miou(lab[i], pred[i], 20)
                     miou_list.append(miou)
+                del x, y, out, pred, lab
+                torch.cuda.empty_cache()
         miou = np.mean(miou_list)
         print('mIoU = %.3f' % (miou * 100))
+
+
+colormap = {
+    0: (0, 0, 0),
+    1: (128, 64, 128),
+    2: (244, 35, 232),
+    3: (70, 70, 70),
+    4: (102, 102, 156),
+    5: (190, 153, 153),
+    6: (153, 153, 153),
+    7: (250, 170, 30),
+    8: (220, 220, 0),
+    9: (107, 142, 35),
+    10: (152, 251, 152),
+    11: (70, 130, 180),
+    12: (220, 20, 60),
+    13: (255, 0, 0),
+    14: (0, 0, 142),
+    15: (0, 0, 70),
+    16: (0, 60, 100),
+    17: (0, 80, 100),
+    18: (0, 0, 230),
+    19: (119, 11, 32)
+}
+
+
+class CityscapesDemo:
+    def __init__(self, root, net):
+        self.device = torch.device(
+            'cuda' if torch.cuda.is_available() else 'cpu')
+
+        net.eval()
+        net.to(self.device)
+        self.net = net
+
+        dirs = ['stuttgart_00', 'stuttgart_01', 'stuttgart_02']
+        paths = []
+        for d in dirs:
+            path0 = os.path.join(root, 'leftImg8bit/demoVideo', d)
+            files = sorted(os.listdir(path0))
+            for f in files:
+                if f[-3:] == 'png':
+                    path = os.path.join(path0, f)
+                    paths.append(path)
+        self.paths = paths
+
+        self.inference()
+
+    def inference(self):
+        preds = []
+        for path in tqdm(self.paths):
+            image = Image.open(path)
+
+            x = np.array(image, np.float32).transpose(2, 0, 1) / 255
+            x = torch.from_numpy(x).unsqueeze(0)
+
+            with torch.no_grad():
+                x = x.to(self.device)
+                out = self.net(x)['out']
+                pred = torch.argmax(out, 1)
+                pred = pred.cpu().numpy()[0]
+
+            pred = pred.astype(np.uint8)
+            preds.append(pred)
+
+        self.preds = preds
+
+    def make_video(self, video_path):
+        fourcc = cv2.VideoWriter_fourcc(*'mp4v')
+        fps = 30
+        w, h = 1920, 1080
+        video = cv2.VideoWriter(video_path, fourcc, fps, (w, h))
+
+        for i in tqdm(range(len(self.preds))):
+            image = cv2.imread(self.paths[i])
+            pred = self.preds[i]
+            H, W = pred.shape
+            view = np.zeros((H, W, 3), dtype=np.uint8)
+            for k in colormap.keys():
+                mask = k == pred
+                view[mask] = colormap[k]
+            view = cv2.cvtColor(view, cv2.COLOR_RGB2BGR)
+
+            overlap = (image * 0.666 + view * 0.333).astype(np.uint8)
+            overlap = cv2.resize(overlap, (w, h))
+            video.write(overlap)
+
+        video.release()
