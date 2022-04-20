0eb8681a0a368e2e0cf80215f51607a5459f2624
sjjeong <sjjeong94@gmail.com>
2022-04-20 23:18:44 +0900

    add 1-epoch training results

--- README.md
+++ README.md
@@ -5,3 +5,12 @@ This is a PyTorch implementation of semantic segmentation models on [comma10k da
 ## Dataset
 - [Cityscapes](https://www.cityscapes-dataset.com/)
 - [comma10k](https://github.com/commaai/comma10k)
+
+
+## comma10k
+
+### 1-Epoch Training Results
+| Case | Model                     | Duration (s) | Train Loss | Val Loss |
+| ---- | ------------------------- | ------------ | ---------- | -------- |
+| 000  | lraspp_mobilenet_v3_large | 1419.1662    | 0.1236     | 0.1616   |
+| 001  | fcn_resnet50              | 1796.9648    | 0.1395     | 0.1984   |
---
+++ benchmarks.py
@@ -0,0 +1,107 @@
+import os
+import time
+import torch
+import random
+import numpy as np
+import torch.nn as nn
+from torchvision.models.segmentation import lraspp_mobilenet_v3_large, fcn_resnet50, deeplabv3_mobilenet_v3_large
+from dataset import Comma10k
+import transforms as T
+from tqdm import tqdm
+
+
+def set_seed(seed):
+    random.seed(seed)
+    os.environ['PYTHONHASHSEED'] = str(seed)
+    np.random.seed(seed)
+    torch.manual_seed(seed)
+    if torch.cuda.is_available():
+        torch.cuda.manual_seed_all(seed)
+        torch.backends.cudnn.deterministic = True
+
+
+def get_comm10k(root, batch_size, num_workers=0, pin_memory=False):
+
+    train_dataset = Comma10k('../comma10k', True)
+    val_dataset = Comma10k('../comma10k', False)
+
+    train_loader = torch.utils.data.DataLoader(
+        dataset=train_dataset, batch_size=batch_size, shuffle=True, num_workers=num_workers, pin_memory=pin_memory)
+    val_loader = torch.utils.data.DataLoader(
+        dataset=val_dataset, batch_size=batch_size, shuffle=False, num_workers=num_workers, pin_memory=pin_memory)
+
+    return train_loader, val_loader
+
+
+def benchmarks(root, net):
+    set_seed(1234)
+    os.makedirs(root, exist_ok=True)
+
+    learning_rate = 0.0003
+    weight_decay = 0
+    batch_size = 2
+
+    dataset = 'Comma10k'
+
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+    net = net.to(device)
+
+    optimizer = torch.optim.AdamW(
+        net.parameters(),
+        lr=learning_rate,
+        weight_decay=weight_decay)
+
+    train_loader, val_loader = get_comm10k(
+        '../comma10k', batch_size, num_workers=4, pin_memory=True)
+
+    print(net)
+    print(device)
+    print(optimizer)
+
+    t0 = time.time()
+    scaler = torch.cuda.amp.GradScaler()
+    criterion = nn.CrossEntropyLoss()
+    net.train()
+    losses = 0
+    for i, (x, y) in enumerate(tqdm(train_loader)):
+        x = x.to(device)
+        y = y.to(device)
+        optimizer.zero_grad()
+
+        with torch.cuda.amp.autocast():
+            out = net(x)['out']
+            loss = criterion(out, y)
+        scaler.scale(loss).backward()
+        scaler.step(optimizer)
+        scaler.update()
+
+        losses += loss.detach()
+    loss_train = losses / len(train_loader)
+    t1 = time.time()
+    duration = t1 - t0
+    print('loss train : ', loss_train)
+
+    net.eval()
+    losses = 0
+    for i, (x, y) in enumerate(tqdm(val_loader)):
+        with torch.no_grad():
+            x = x.to(device)
+            y = y.to(device)
+
+            out = net(x)['out']
+            loss = criterion(out, y)
+
+            losses += loss.detach()
+
+    loss_val = losses / len(val_loader)
+    print('loss val : ', loss_val)
+
+    print('| %12.4f | %12.4f | %12.4f |' % (duration, loss_train, loss_val))
+
+    model_path = os.path.join(root, 'model.pth')
+    torch.save(net.state_dict(), model_path)
+
+
+if __name__ == '__main__':
+    #benchmarks('results/000', lraspp_mobilenet_v3_large(num_classes=5))
+    benchmarks('results/001', fcn_resnet50(num_classes=5))
