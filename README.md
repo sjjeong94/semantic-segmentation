8de7f61825aec6b0373a3267ac6b5cf9a94f3968
sjjeong <sjjeong94@gmail.com>
2022-04-27 17:52:49 +0900

    add augmentation results

--- README.md
+++ README.md
@@ -9,6 +9,8 @@ This is a PyTorch implementation of semantic segmentation models on [comma10k da
 
 ## comma10k
 
+<br>
+
 ### 1-Epoch Training Results
 | Case  | Model                               | Duration (s) | Train Loss | Val Loss |
 | ----- | ----------------------------------- | ------------ | ---------- | -------- |
@@ -17,3 +19,19 @@ This is a PyTorch implementation of semantic segmentation models on [comma10k da
 | 002   | deeplabv3_mobilenet_v3_large (bs=4) | 1812.6592    | 0.1099     | 0.0848   |
 | 002_1 | bs=8                                | 1731.3506    | 0.1141     | 0.0814   |
 | 002_2 | input normalization                 | 1688.1650    | 0.1144     | 0.0795   |
+
+<br>
+
+### 5-Epoch Training Results
+| Case  | Model                        | 1-Epoch (s) | Train Loss | Val Loss |
+| ----- | ---------------------------- | ----------- | ---------- | -------- |
+| 002_3 | deeplabv3_mobilenet_v3_large | 1682.5930   | 0.0503     | 0.0578   |
+| 002_4 | random flip                  | 1685.1737   | 0.0523     | 0.0581   |
+| 002_5 | random crop                  | 413.7552    | 0.0935     | 0.1110   |
+
+<br>
+
+### 30-Epoch Training Results
+| Case  | Model       | 1-Epoch (s) | Train Loss | Val Loss |
+| ----- | ----------- | ----------- | ---------- | -------- |
+| 002_5 | random crop | 414.1535    | 0.0598     | 0.0880   |
--- benchmarks.py
+++ benchmarks.py
@@ -26,7 +26,10 @@ def benchmarks(
     learning_rate=0.0003,
     weight_decay=0,
     batch_size=2,
+    epochs=1,
     normalize=False,
+    random_crop=False,
+    random_flip=False,
     num_workers=4,
     pin_memory=True,
 ):
@@ -41,8 +44,17 @@ def benchmarks(
         lr=learning_rate,
         weight_decay=weight_decay)
 
-    train_dataset = Comma10k('../comma10k', True, normalize=True)
-    val_dataset = Comma10k('../comma10k', False, normalize=True)
+    T_train = []
+    if random_crop:
+        T_train.extend([T.RandomResize(512, 1024), T.RandomCrop(512)])
+    if random_flip:
+        T_train.append(T.RandomHorizontalFlip(0.5))
+    T_train = T.Compose(T_train)
+    print(T_train)
+
+    train_dataset = Comma10k(
+        '../comma10k', True, normalize=normalize, transform=T_train)
+    val_dataset = Comma10k('../comma10k', False, normalize=normalize)
 
     train_loader = torch.utils.data.DataLoader(
         dataset=train_dataset, batch_size=batch_size, shuffle=True, num_workers=num_workers, pin_memory=pin_memory)
@@ -53,48 +65,51 @@ def benchmarks(
     print(device)
     print(optimizer)
 
-    t0 = time.time()
     scaler = torch.cuda.amp.GradScaler()
     criterion = nn.CrossEntropyLoss()
-    net.train()
-    losses = 0
-    for i, (x, y) in enumerate(tqdm(train_loader)):
-        x = x.to(device)
-        y = y.to(device)
-        optimizer.zero_grad()
 
-        with torch.cuda.amp.autocast():
-            out = net(x)['out']
-            loss = criterion(out, y)
-        scaler.scale(loss).backward()
-        scaler.step(optimizer)
-        scaler.update()
-
-        losses += loss.detach()
-    loss_train = losses / len(train_loader)
-    t1 = time.time()
-    duration = t1 - t0
-    print('loss train : ', loss_train)
-
-    net.eval()
-    losses = 0
-    for i, (x, y) in enumerate(tqdm(val_loader)):
-        with torch.no_grad():
+    for epoch in range(epochs):
+        t0 = time.time()
+        net.train()
+        losses = 0
+        for i, (x, y) in enumerate(tqdm(train_loader)):
             x = x.to(device)
             y = y.to(device)
+            optimizer.zero_grad()
 
-            out = net(x)['out']
-            loss = criterion(out, y)
+            with torch.cuda.amp.autocast():
+                out = net(x)['out']
+                loss = criterion(out, y)
+            scaler.scale(loss).backward()
+            scaler.step(optimizer)
+            scaler.update()
 
             losses += loss.detach()
+        loss_train = losses / len(train_loader)
+        t1 = time.time()
+        duration = t1 - t0
+        print('loss train : ', loss_train)
 
-    loss_val = losses / len(val_loader)
-    print('loss val : ', loss_val)
+        net.eval()
+        losses = 0
+        for i, (x, y) in enumerate(tqdm(val_loader)):
+            with torch.no_grad():
+                x = x.to(device)
+                y = y.to(device)
 
-    print('| %12.4f | %12.4f | %12.4f |' % (duration, loss_train, loss_val))
+                out = net(x)['out']
+                loss = criterion(out, y)
 
-    model_path = os.path.join(root, 'model.pth')
-    torch.save(net.state_dict(), model_path)
+                losses += loss.detach()
+
+        loss_val = losses / len(val_loader)
+        print('loss val : ', loss_val)
+
+        print('| epoch %3d | %12.4f | %12.4f | %12.4f |' %
+              (epoch, duration, loss_train, loss_val))
+
+        model_path = os.path.join(root, 'model_%03d.pth' % epoch)
+        torch.save(net.state_dict(), model_path)
 
 
 if __name__ == '__main__':
@@ -104,5 +119,13 @@ if __name__ == '__main__':
     #          deeplabv3_mobilenet_v3_large(num_classes=5), batch_size=4)
     # benchmarks('results/002_1',
     #           deeplabv3_mobilenet_v3_large(num_classes=5), batch_size=8)
-    benchmarks('results/002_2',
-               deeplabv3_mobilenet_v3_large(num_classes=5), batch_size=8)
+    # benchmarks('results/002_2',
+    #           deeplabv3_mobilenet_v3_large(num_classes=5), batch_size=8, normalize=True)
+    # benchmarks('results/002_3',
+    #           deeplabv3_mobilenet_v3_large(num_classes=5), batch_size=8, epochs=5, normalize=True)
+    # benchmarks('results/002_4',
+    #           deeplabv3_mobilenet_v3_large(num_classes=5), batch_size=8, epochs=5, normalize=True, random_flip=True)
+    # benchmarks('results/002_5',
+    #           deeplabv3_mobilenet_v3_large(num_classes=5), batch_size=8, epochs=5, normalize=True, random_crop=True)
+    benchmarks('results/002_5',
+               deeplabv3_mobilenet_v3_large(num_classes=5), batch_size=8, epochs=30, normalize=True, random_crop=True)
